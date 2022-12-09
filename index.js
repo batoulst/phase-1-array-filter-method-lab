7334e8486e427d02a193360c15bb7def02d236a0
batoul <batoul.tenbakji@gmail.com>
2022-12-09 20:49:57 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
 // Code your solution here
+function findMatching(drivers,str){
+    const res=drivers.filter((driver)=>driver.toLowerCase()==str.toLowerCase())
+ return res;
+}
+
+function fuzzyMatch(drivers,str){
+    const res=drivers.filter((driver)=>driver.startsWith(str))
+ return res;
+}
+function matchName(drivers,str){
+    const res=drivers.filter((driver)=>driver.name===str)
+ return res;
+}
